b284ae6a8802a6ef9047b4828dbd4a716482b02e
Maximilian Lipp <47270471+Flapmax@users.noreply.github.com>
2019-09-07 00:54:17 +0200

    Add files via upload

--- touchboard_MaxPi4.py
+++ touchboard_MaxPi4.py
@@ -1,16 +1,16 @@
-# MaxPi 1
+#MaxPi 2
 import time
 import board
 from digitalio import DigitalInOut, Direction
 import vlc
 from firebase import firebase
 
-# calling firebase getting the ID and printing the result
+#calling firebase getting the ID and printing the result
 firebase = firebase.FirebaseApplication('https://bowall.firebaseio.com/')
-result = firebase.get('/checkValue', 'checkID')
+result = firebase.get('/checkValue','checkID')
 print(result)
 
-# importing the Sounds
+#importing the Sounds
 location = vlc.MediaPlayer("Localistation.wav")
 Error = vlc.MediaPlayer("Error.wav")
 Success = vlc.MediaPlayer("Success.wav")
@@ -25,19 +25,21 @@ pad0.direction = Direction.INPUT
 
 pad0_already_pressed = True
 
+
 while True:
 
+
     if pad0.value and not pad0_already_pressed:
-        # playing the sound on touch
+        #playing the sound on touch
         Success.play()
         print("Pad 0 Pressed")
-        # Changing the ID to 1
-        post = firebase.patch('/checkValue', {'checkID': 1})
+        #Changing the ID to 1
+        post = firebase.patch('/checkValue',{'checkID': 1})
     pad0_already_pressed = pad0.value
 
     time.sleep(0.1)
-
-    result = firebase.get('/checkValue', 'checkID')
-
+    
+    result = firebase.get('/checkValue','checkID') 
+    
     if result == 1:
         exit()
