d97f9b81b08f88416fe54e27aa9559551a552a4c
Maximilian Lipp <47270471+Flapmax@users.noreply.github.com>
2019-09-08 15:14:09 +0200

    Add files via upload

---
+++ touchboard_test_02.py
@@ -0,0 +1,59 @@
+def main():
+    # Test Code
+    import time
+    import board
+    from digitalio import DigitalInOut, Direction
+    import vlc
+    from firebase import firebase
+
+    # calling firebase getting the ID and printing the result
+    firebase = firebase.FirebaseApplication('https://bowall.firebaseio.com/')
+    result = firebase.get('/checkValue', 'checkID')
+    print(result)
+
+    #restart als variable
+    restart = 1
+
+    # importing the Sounds
+    location = vlc.MediaPlayer("Localistation.wav")
+    Error = vlc.MediaPlayer("Error.wav")
+    Success = vlc.MediaPlayer("Success.wav")
+    Celebration = vlc.MediaPlayer("Celebration.wav")
+
+    # set the GPIO input pins
+    pad0_pin = board.D4
+
+    pad0 = DigitalInOut(pad0_pin)
+
+    pad0.direction = Direction.INPUT
+
+    pad0_already_pressed = True
+
+    def function():
+        if result == 1:
+            location.play()
+
+            while True:
+
+                if pad0.value and not pad0_already_pressed:
+                    # playing the sound on touch
+                    Success.play()
+                    print("Pad 0 Pressed")
+                    # Changing the ID to 1
+                    post = firebase.patch('/checkValue', {'checkID': 2})
+                pad0_already_pressed = pad0.value
+
+
+                time.sleep(0.1)
+
+                result = firebase.get('/checkValue', 'checkID')
+
+
+
+        if restart == 1:
+            print("looping")
+            function()
+
+main()
+function()
+
