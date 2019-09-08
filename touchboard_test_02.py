7b582f36bec708eefb8ce229ca660810dbad749d
Maximilian Lipp <47270471+Flapmax@users.noreply.github.com>
2019-09-08 16:53:36 +0200

    Add files via upload

--- touchboard_test_02.py
+++ touchboard_test_02.py
@@ -1,53 +1,53 @@
 # restart als variable
 restart = 1
+# Test Code
+import time
+import board
+from digitalio import DigitalInOut, Direction
+import vlc
+from firebase import firebase
+
+# calling firebase getting the ID and printing the result
+firebase = firebase.FirebaseApplication('https://bowall.firebaseio.com/')
+result = firebase.get('/checkValue', 'checkID')
+print(result)
+
+# importing the Sounds
+location = vlc.MediaPlayer("Localistation.wav")
+Error = vlc.MediaPlayer("Error.wav")
+Success = vlc.MediaPlayer("Success.wav")
+Celebration = vlc.MediaPlayer("Celebration.wav")
+
+# set the GPIO input pins
+pad0_pin = board.D4
+
+pad0 = DigitalInOut(pad0_pin)
+
+pad0.direction = Direction.INPUT
+
+pad0_already_pressed = True
+
 
 def main():
+    global result
 
-    # Test Code
-    import time
-    import board
-    from digitalio import DigitalInOut, Direction
-    import vlc
-    from firebase import firebase
+    if result == 1:
+        location.play()
 
-    # calling firebase getting the ID and printing the result
-    firebase = firebase.FirebaseApplication('https://bowall.firebaseio.com/')
-    result = firebase.get('/checkValue', 'checkID')
-    print(result)
+        while True:
+            global pad0
+            global pad0_already_pressed
 
-    # importing the Sounds
-    location = vlc.MediaPlayer("Localistation.wav")
-    Error = vlc.MediaPlayer("Error.wav")
-    Success = vlc.MediaPlayer("Success.wav")
-    Celebration = vlc.MediaPlayer("Celebration.wav")
-
-    # set the GPIO input pins
-    pad0_pin = board.D4
-
-    pad0 = DigitalInOut(pad0_pin)
-
-    pad0.direction = Direction.INPUT
-
-    pad0_already_pressed = True
-
-    while restart == 1:
-        global result
-
-        if result == 1:
-            location.play()
-
-            while True:
-
-                if pad0.value and not pad0_already_pressed:
-                    # playing the sound on touch
-                    Success.play()
-                    print("Pad 0 Pressed")
-                    # Changing the ID to 1
-                    post = firebase.patch('/checkValue', {'checkID': 2})
+            if pad0.value and not pad0_already_pressed:
+                # playing the sound on touch
+                Success.play()
+                print("Pad 0 Pressed")
+                # Changing the ID to 1
+                post = firebase.patch('/checkValue', {'checkID': 2})
                 pad0_already_pressed = pad0.value
 
                 result = firebase.get('/checkValue', 'checkID')
 
-main();
 
-
+while True:
+    main()
